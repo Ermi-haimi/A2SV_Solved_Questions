71a82aa69bf03f24e97aa180f8dbbdd7766ae088
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-02-25 17:39:01 +0300

    lc 881 solution

---
+++ 881. Boats to Save People.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        count = 0
+        left = 0
+        right = len(people)-1
+        while(left <= right):
+            if(people[right]+people[left] <= limit):
+                left+=1
+            right-=1
+            count+=1
+        return count
