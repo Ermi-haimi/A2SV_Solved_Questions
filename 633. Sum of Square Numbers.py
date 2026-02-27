ab749fa79fd14d23561e7faf1c8422f789fc7837
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-02-27 19:33:34 +0300

    lc 633 solution

---
+++ 633. Sum of Square Numbers.py
@@ -0,0 +1,15 @@
+from math import sqrt
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left = 0
+        right = int(sqrt(c))
+        while(left <= right):
+            curr = (left**2) + (right**2)
+            if curr == c:
+                return True
+            elif(curr < c):
+                left+=1
+            else:
+                right-=1
+        
+        return False
