29ad7ceea1a02bf4f3e643583738f9e93907f54a
Ermias Haimanot Tsegaye <ermiashtsegaye@gmail.com>
2026-03-11 18:34:47 +0300

    Add solution for Daily Temperatures

---
+++ leetcode/0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        st = []
+        n = len(temperatures)
+        ans = [0]*n
+        for i in range(n):
+            while(st and temperatures[st[-1]]<temperatures[i]):
+                curr = st.pop()
+                ans[curr] = i-curr
+            st.append(i)
+        
+        return ans
