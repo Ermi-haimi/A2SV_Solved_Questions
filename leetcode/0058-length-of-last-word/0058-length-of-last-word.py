c0a245bc88496b09ee5ac851dc1ecb99f4f518a3
Ermias Haimanot Tsegaye <ermiashtsegaye@gmail.com>
2026-04-12 20:17:13 +0300

    Add solution for Length of Last Word

---
+++ leetcode/0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        n = len(s)
+        count = 0
+        for i in range(n-1,-1,-1):
+            if s[i] != ' ':
+                count+=1
+            else:
+                if count > 0:
+                    break 
+        return count
